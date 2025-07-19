a8f43a74db7c2527e12fb4477410b91f9e2e4d7b
Max16 <max16@naver.com>
2025-07-19 20:51:07 +0900

    #4.1 Login Screen

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/login_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
+
+  void onLoginTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +38,14 @@ class SignUpScreen extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
+              Gaps.v40,
+              AuthButton(text: 'Use phone or email'),
+              Gaps.v16,
+              AuthButton(text: 'Continue with Facebook'),
+              Gaps.v16,
+              AuthButton(text: 'Continue with Apple'),
+              Gaps.v16,
+              AuthButton(text: 'Continue with Google'),
             ],
           ),
         ),
@@ -42,11 +58,14 @@ class SignUpScreen extends StatelessWidget {
           children: [
             Text("Already have an account?"),
             Gaps.h5,
-            Text(
-              "Log in",
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontWeight: FontWeight.w600,
+            GestureDetector(
+              onTap: () => onLoginTap(context),
+              child: Text(
+                "Log in",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ],
