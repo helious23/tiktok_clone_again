1b72a38e13a5f228ba8097b9452b50cfd99dd06a
Max16 <max16@naver.com>
2025-07-23 10:09:05 +0900

    #7.6 AnimatedBuilder

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -55,16 +55,14 @@ class _VideoPostState extends State<VideoPost>
       value: 1.5,
       duration: _animationDuration,
     );
-    _animationController.addListener(() {
-      // build method 를 변할때 마다 호출하기 위해 setState
-      setState(() {});
-    });
   }
 
   @override
   void dispose() {
     _videoPlayerController.removeListener(_onVideoChange);
     _videoPlayerController.dispose();
+    _animationController.removeListener(() {});
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -106,8 +104,14 @@ class _VideoPostState extends State<VideoPost>
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
-                child: Transform.scale(
-                  scale: _animationController.value,
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: _animationController.value,
+                      child: child,
+                    );
+                  },
                   child: AnimatedOpacity(
                     opacity: _isPaused ? 1 : 0,
                     duration: _animationDuration,
