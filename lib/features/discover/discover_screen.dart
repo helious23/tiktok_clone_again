9794d4bc46b4f0e94ec1cb2171fbd8450fdc3c06
Max16 <max16@naver.com>
2025-07-27 11:17:33 +0900

    #9.4 Grid item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 final tabs = [
   "Top",
@@ -49,11 +51,68 @@ class DiscoverScreen extends StatelessWidget {
                 crossAxisCount: 2,
                 crossAxisSpacing: Sizes.size10,
                 mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 16,
+                childAspectRatio: 9 / 20,
               ),
-              itemBuilder: (context, index) => Container(
-                color: Colors.teal,
-                child: Center(child: Text("$index")),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                      placeholder: "assets/images/placeholder.jpeg",
+                      image:
+                          "https://picsum.photos/id/${index + 10}/900/1600",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Gaps.v10,
+                  Text(
+                    "This is a very long caption for my tiktok video that i'm going to use to test the layout of the video caption",
+                    style: TextStyle(
+                      fontSize: Sizes.size16 + Sizes.size2,
+                      fontWeight: FontWeight.w600,
+                      height: 1.2,
+                      letterSpacing: -0.5,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Gaps.v8,
+                  DefaultTextStyle(
+                    style: TextStyle(
+                      fontSize: Sizes.size14,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey.shade700,
+                      height: 1.2,
+                      letterSpacing: -0.5,
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: Sizes.size12,
+                          backgroundImage: NetworkImage(
+                            "https://picsum.photos/id/${index + 10}/200/200",
+                          ),
+                        ),
+                        Gaps.h5,
+                        Expanded(
+                          child: Text(
+                            "My avatar is going to be very long",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade700,
+                        ),
+                        Gaps.h4,
+                        Text("1.2M"),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
             for (var tab in tabs.skip(1))
